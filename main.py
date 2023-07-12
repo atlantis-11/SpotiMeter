1719739ab063efce54ea142e3426ff5b3ad65e4a
Yevhenii <atlantis.j11@gmail.com>
2023-07-12 12:06:49 +0300

    Added getting track's features, added help and made output more readable

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import stats
 
 parser = ArgumentParser()
 
-parser.add_argument('--auth', action='store_true')
+parser.add_argument('--auth', action='store_true', help='add this option if you haven\'t authorized yet and tokens.json file is not present')
 parser.add_argument('--top', choices=['tracks', 'artists', 'genres'])
 parser.add_argument('--time-range', choices=['short_term', 'medium_term', 'long_term'])
-parser.add_argument('--limit', choices=[str(i) for i in range(1, 51)])
+parser.add_argument('--limit', help='how many items to return, 1 to 50')
+
+parser.add_argument('--get-features', metavar='TRACK_ID')
 
 args = parser.parse_args()
 
@@ -50,9 +52,16 @@ else:
     except Exception as e:
         print(e)
 
+def print_list(list):
+    for item in list:
+        print(item)
+
 if args.top == 'tracks':
-    print(stats.get_top_tracks(tokens['access_token'], args.time_range, args.limit))
+    print_list(stats.get_top_tracks(tokens['access_token'], args.time_range, args.limit))
 elif args.top == 'artists':
-    print(stats.get_top_artists(tokens['access_token'], args.time_range, args.limit))
+    print_list(stats.get_top_artists(tokens['access_token'], args.time_range, args.limit))
 elif args.top == 'genres':
-    print(stats.get_top_genres(tokens['access_token'], args.time_range, args.limit))
+    print_list(stats.get_top_genres(tokens['access_token'], args.time_range, args.limit))
+
+elif args.get_features is not None:
+    print_list(stats.get_audio_features(tokens['access_token'], args.get_features))
--- stats.py
+++ stats.py
@@ -26,7 +26,8 @@ def get_top_tracks(access_token: str, time_range=None, limit=None):
             'name': track['name'],
             'artists': [d['name'] for d in track['artists']],
             'album': track['album']['name'],
-            'popularity': track['popularity']
+            'popularity': track['popularity'],
+            'id': track['id']
         })
 
     return res
@@ -67,3 +68,16 @@ def get_top_genres(access_token, time_range=None, limit=None):
     genres = [i[0] for i in sorted(genres.items(), key=lambda x:x[1], reverse=True)[:limit]]
 
     return genres
+
+def get_audio_features(access_token, track_id):
+    url = f'https://api.spotify.com/v1/audio-features/{track_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise HTTPError()
+    
+    return(response.json())
