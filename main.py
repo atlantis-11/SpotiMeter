270a4dfdd957c57f9ce512dbff20073f1b6d06ba
Yevhenii <atlantis.j11@gmail.com>
2023-07-12 17:14:25 +0300

    Refactoring

--- main.py
+++ main.py
@@ -4,61 +4,79 @@ import authorization
 import stats
 from custom_prints import print_list, print_dict
 
-parser = ArgumentParser()
+def parse_arguments():
+    parser = ArgumentParser()
 
-parser.add_argument('--auth', action='store_true', help='add this option if you haven\'t authorized yet and tokens.json file is not present')
-parser.add_argument('--top', choices=['tracks', 'artists', 'genres'])
-parser.add_argument('--time-range', choices=['short_term', 'medium_term', 'long_term'])
-parser.add_argument('--limit', help='how many items to return, 1 to 50')
+    parser.add_argument('--auth', action='store_true', help='add this option if you haven\'t authorized yet and tokens.json file is not present')
+    parser.add_argument('--top', choices=['tracks', 'artists', 'genres'])
+    parser.add_argument('--time-range', choices=['short_term', 'medium_term', 'long_term'])
+    parser.add_argument('--limit', help='how many items to return, 1 to 50')
 
-parser.add_argument('--get-features', metavar='TRACK_ID')
+    parser.add_argument('--get-features', metavar='TRACK_ID')
 
-args = parser.parse_args()
+    return parser.parse_args()
 
-tokens = {} # access_token and refresh_token
-scope = 'user-top-read'
-
-with open('credentials.json', 'r') as f:
-    credentials = json.load(f)
-
-if not args.auth:
+def load_from_json(filename):
     try:
-        with open('tokens.json', 'r') as f:
-            tokens = json.load(f)
+        with open(filename, 'r') as f:
+            return json.load(f)
+        
+    except FileNotFoundError:
+        raise FileNotFoundError(f'No {filename} file')
+    
+def save_to_json(filename, dict):
+    try:
+        with open(filename, 'w') as f:
+            json.dump(dict, f)
 
-        if authorization.check_access_token_validity(tokens['access_token']):
+    except FileNotFoundError:
+        raise FileNotFoundError(f'No {filename} file')
+
+def main():
+    args = parse_arguments()
+
+    credentials = load_from_json('credentials.json')
+
+    tokens = {} # access_token and refresh_token
+    scope = 'user-top-read'
+
+    if args.auth:
+        try:
+            tokens = authorization.get_tokens(credentials['client_id'], credentials['client_secret'], scope)
+
+            save_to_json('tokens.json', tokens)
+
             print('Authorized successfully')
 
-        else:
-            print('Access token not valid, attemting to refresh it...')
-            tokens['access_token'] = authorization.get_refreshed_access_token(credentials['client_id'], credentials['client_secret'], tokens['refresh_token'])
+        except Exception as e:
+            print(e)
 
-            with open('tokens.json', 'w') as f:
-                json.dump(tokens, f)
+    else:
+        try:
+            tokens = load_from_json('tokens.json')
 
-            print('Authorized successfully')
+            if authorization.check_access_token_validity(tokens['access_token']):
+                print('Authorized successfully')
 
-    except Exception as e:
-        print(e)
+            else:
+                print('Access token not valid, attemting to refresh it...')
+                tokens['access_token'] = authorization.get_refreshed_access_token(credentials['client_id'], credentials['client_secret'], tokens['refresh_token'])
 
-else:
-    try:
-        tokens = authorization.get_tokens(credentials['client_id'], credentials['client_secret'], scope)
+                save_to_json('tokens.json', tokens)
 
-        with open('tokens.json', 'w') as f:
-            json.dump(tokens, f)
+                print('Authorized successfully')
 
-        print('Authorized successfully')
+        except Exception as e:
+            print(e)
 
-    except Exception as e:
-        print(e)
+    if args.get_features:
+        print_dict(stats.get_audio_features(tokens['access_token'], args.get_features))
 
-if args.top == 'tracks':
-    print_list(stats.get_top_tracks(tokens['access_token'], args.time_range, args.limit))
-elif args.top == 'artists':
-    print_list(stats.get_top_artists(tokens['access_token'], args.time_range, args.limit))
-elif args.top == 'genres':
-    print_list(stats.get_top_genres(tokens['access_token'], args.time_range, args.limit))
+    elif args.top:
+        top_functions = {'tracks': stats.get_top_tracks, 'artists': stats.get_top_artists, 'genres': stats.get_top_genres}
+        get_top = top_functions[args.top]
 
-elif args.get_features is not None:
-    print_dict(stats.get_audio_features(tokens['access_token'], args.get_features))
+        print_list(get_top(tokens['access_token'], args.time_range, args.limit))
+
+if __name__ == '__main__':
+    main()
--- stats.py
+++ stats.py
@@ -2,6 +2,27 @@ import requests
 from requests.exceptions import HTTPError
 import urllib
 
+def _get_top_items(access_token, type, time_range, limit): # type is either artists or tracks
+    data = {}
+
+    if time_range is not None:
+        data['time_range'] = time_range
+
+    if limit is not None:
+        data['limit'] = limit
+
+    url = f'https://api.spotify.com/v1/me/top/{type}?' + urllib.parse.urlencode(data)
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise HTTPError()
+    
+    return response.json()['items']
+
 def get_top_artists(access_token: str, time_range=None, limit=None):
     items = _get_top_items(access_token, 'artists', time_range, limit)
 
@@ -32,28 +53,7 @@ def get_top_tracks(access_token: str, time_range=None, limit=None):
 
     return res
 
-def _get_top_items(access_token, type, time_range, limit):
-    data = {}
-
-    if time_range is not None:
-        data['time_range'] = time_range
-
-    if limit is not None:
-        data['limit'] = limit
-
-    url = f'https://api.spotify.com/v1/me/top/{type}?' + urllib.parse.urlencode(data)
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        raise HTTPError()
-    
-    return response.json()['items']
-
-def get_top_genres(access_token, time_range=None, limit=None):
+def get_top_genres(access_token: str, time_range=None, limit=None):
     if (limit is None):
         limit = 5
     else:
@@ -69,7 +69,7 @@ def get_top_genres(access_token, time_range=None, limit=None):
 
     return genres
 
-def get_audio_features(access_token, track_id):
+def get_audio_features(access_token: str, track_id: str):
     url = f'https://api.spotify.com/v1/audio-features/{track_id}'
     headers = {
         'Authorization': f'Bearer {access_token}'
